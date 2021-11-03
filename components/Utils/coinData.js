0fdaf516f737a006e98e0821c1e9b1e3d1a80abe
Farhaan <farhaanthestar@gmail.com>
2021-11-03 11:48:57 +0530

    Added custom Icon API

--- components/Utils/coinData.js
+++ components/Utils/coinData.js
@@ -1,43 +1,43 @@
 const coinData = {
   Bitcoin: {
     symbol: "BTC",
-    img: "https://cryptoicon-api.vercel.app/api/icon/btc",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/btc",
   },
   Ethereum: {
     symbol: "ETH",
-    img: "https://cryptoicon-api.vercel.app/api/icon/eth",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/eth",
   },
   Cardano: {
     symbol: "ADA",
-    img: "https://cryptoicon-api.vercel.app/api/icon/ada",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/ada",
   },
   Dogecoin: {
     symbol: "DOGE",
-    img: "https://cryptoicon-api.vercel.app/api/icon/doge",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/doge",
   },
   Shibainu: {
     symbol: "SHIB",
-    img: "https://cryptoicon-api.vercel.app/api/icon/usdt", //no image available
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/usdt", //no image available
   },
   Tether: {
     symbol: "USDT",
-    img: "https://cryptoicon-api.vercel.app/api/icon/usdt",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/usdt",
   },
   Litecoin: {
     symbol: "LTC",
-    img: "https://cryptoicon-api.vercel.app/api/icon/ltc",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/ltc",
   },
   Monero: {
     symbol: "XMR ",
-    img: "https://cryptoicon-api.vercel.app/api/icon/xmr",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/xmr",
   },
   Polkadot: {
     symbol: "DOT",
-    img: "https://cryptoicon-api.vercel.app/api/icon/dot",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/dot",
   },
   Stellar: {
     symbol: "XLM",
-    img: "https://cryptoicon-api.vercel.app/api/icon/xlm",
+    img: "https://cryptoflash-icons-api.herokuapp.com/svg/icon/xlm",
   },
 };
 
