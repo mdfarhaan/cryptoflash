a3a43a39b58d1d12fdb8f460b9c0e18dc9b3444a
Farhaan <farhaanthestar@gmail.com>
2021-11-07 16:11:32 +0530

    Fix: Roundoff error

--- components/Dashboard/Main/Dashboard.js
+++ components/Dashboard/Main/Dashboard.js
@@ -93,10 +93,13 @@ function Dashboard(props) {
             Holdings = Holdings - coinDoc.quantity;
             Value = Value - coinDoc.totalSpent;
           }
+          console.log("iamhere");
           if (Number(Holdings) === Holdings && Holdings % 1 === 0) {
+            console.log(Holdings);
             Holdings = parseInt(Holdings);
           } else {
-            Holdings = parseFloat(Holdings.toFixed(4));
+            Holdings = parseFloat(Holdings);
+            console.log(Holdings);
           }
           setTimeout(() => {
             // Update TableCoinData
