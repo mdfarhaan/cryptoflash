fe23cff02ef02e87a0b407578f2eb8bd329d43af
Farhaan <farhaanthestar@gmail.com>
2021-11-11 19:27:57 +0530

    Update Dashboard.js

--- components/Dashboard/Main/Dashboard.js
+++ components/Dashboard/Main/Dashboard.js
@@ -117,7 +117,7 @@ function Dashboard(props) {
         }, 1000);
       });
     });
-  }, []); //data
+  }, [data]); //data
 
   //Fetch Coin Data from Table collection
   const fetchTableData = () => {
