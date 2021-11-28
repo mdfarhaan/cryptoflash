02ab4b74e905c23d165bd305f7d5cf3ec8734a21
Farhaan <farhaanthestar@gmail.com>
2021-11-29 00:06:32 +0530

    :wrench: Add env

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
+require("dotenv").config;
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_IxolyWcGDWCbKseZa0E3gyMtBQnqIes",
-  authDomain: "crypto-portfolio-cf86b.firebaseapp.com",
-  projectId: "crypto-portfolio-cf86b",
-  storageBucket: "crypto-portfolio-cf86b.appspot.com",
-  messagingSenderId: "1028742613963",
-  appId: "1:1028742613963:web:0ee04012e0dc34efe9844e",
-  measurementId: "G-94FQLQLH7F",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  reactStrictMode: true,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      os: false,
+    };
+
+    return config;
+  },
 };
-future: {
-  webpack5: true;
-}
